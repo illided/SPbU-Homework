25cad4ea0784d781c4e06379828d2feb80946a06
illided <utkin.ilya.nickolaevich@gmail.com>
2020-05-20 12:44:14 +0300

    Tests were added and HashTable was refactored (put function was changed and some analyze functions were added)

--- src/main/kotlin/spbuhomework/hw4/task1/HashTable.kt
+++ src/main/kotlin/spbuhomework/hw4/task1/HashTable.kt
@@ -2,33 +2,61 @@ package spbuhomework.hw4.task1
 
 import kotlin.math.abs
 
-class HashTable<K : Comparable<K>, V>(
-    var arraySize: Int = 5,
-    var extendThreshold: Int = 11,
-    var extendFactor: Int = 2
-) {
+class HashTable<K : Comparable<K>, V> {
+    private var arraySize: Int = 5
+    private var conflictNumber = 0
+
+    var extendThreshold: Int = 5
+        private set(value) {
+            if (value < 2) {
+                throw IllegalArgumentException("Hashtable extend threshold cannot be less than 2")
+            } else {
+                field = value
+            }
+        }
+
+    var extendFactor: Double = 2.0
+        set(value) {
+            if (value <= 1) {
+                throw IllegalArgumentException("Hashtable extend factor cannot be less or equal 1")
+            } else {
+                field = value
+            }
+        }
+
     var size = 0
-    private set
+        private set
+
     private var arrayOfList: Array<MutableList<Entry<K, V>>> = Array(arraySize) { mutableListOf<Entry<K, V>>() }
 
     var hashFunction: (K) -> Int = { key: K -> key.hashCode() }
         set(value) {
             field = value
-            extend(arraySize)
+            refill()
         }
+
+    val loadFactor: Double
+        get() = size / ((arraySize * extendThreshold).toDouble())
 
     private fun getHash(key: K): Int {
         return abs(hashFunction(key)) % arraySize
     }
 
-    private fun extend(newSize: Int) {
-        val newArrayOfList: Array<MutableList<Entry<K, V>>> = Array(newSize) { mutableListOf<Entry<K, V>>() }
+    private fun refill(newArraySize: Int = arraySize, newExtendThreshold: Int = extendThreshold) {
+        val newArrayOfList: Array<MutableList<Entry<K, V>>> = Array(newArraySize) { mutableListOf<Entry<K, V>>() }
+        conflictNumber = 0
+        var targetList: MutableList<Entry<K, V>>
         for (listOfEntry in arrayOfList) {
             for (entry in listOfEntry) {
-                putInArrayOfList(newArrayOfList, entry.key, entry.value)
+                targetList = newArrayOfList[getHash(entry.key)]
+                if (targetList.isNotEmpty()) {
+                    conflictNumber++
+                }
+                targetList.add(entry)
             }
         }
-        arraySize = newSize
+        arraySize = newArraySize
+        extendThreshold = newExtendThreshold
         arrayOfList = newArrayOfList
     }
 
@@ -36,8 +64,8 @@ class HashTable<K : Comparable<K>, V>(
         return arrayOfList[getHash(key)].find { it.key == key }
     }
 
-    fun get(key: K): V?{
-        if (isContains(key)){
+    fun get(key: K): V? {
+        if (isContains(key)) {
             return find(key)?.value
         } else {
             throw IndexOutOfBoundsException("Don't have an item with this key in hashtable")
@@ -48,17 +76,16 @@ class HashTable<K : Comparable<K>, V>(
         return find(key) != null
     }
 
-    private fun putInArrayOfList(arrayOfList: Array<MutableList<Entry<K, V>>>, key: K, value: V) {
-        val targetList = arrayOfList[getHash(key)]
-        targetList.add(Entry(key, value))
-        if (targetList.size >= extendThreshold) {
-            extend(extendFactor * arraySize)
-        }
-    }
-
     fun put(key: K, value: V) {
         if (!isContains(key)) {
-            putInArrayOfList(arrayOfList, key, value)
+            val targetList = arrayOfList[getHash(key)]
+            if (targetList.isNotEmpty()) {
+                conflictNumber++
+            }
+            targetList.add(Entry(key, value))
+            if (targetList.size > extendThreshold) {
+                refill((extendFactor * arraySize).toInt(), (extendFactor * extendThreshold).toInt())
+            }
             size++
         }
     }
@@ -72,8 +99,27 @@ class HashTable<K : Comparable<K>, V>(
         }
     }
 
-    fun getStatisticString() {
-
+    fun getStatisticString(): String {
+        var maximumListLength: Int = 0
+        for (listOfEntry in arrayOfList) {
+            maximumListLength = maxOf(maximumListLength, listOfEntry.size)
+        }
+        return "Current size: $size\n" +
+                "Potentially maximum number of cells: ${arraySize * extendThreshold}\n" +
+                "Current load factor: $loadFactor\n" +
+                "Number of conflicts since last refill: $conflictNumber\n" +
+                "Maximum length of conflict list: $maximumListLength"
     }
 
+    fun getStructuralString(): String {
+        var outputString = ""
+        for (i in arrayOfList.indices) {
+            outputString += "$i -> "
+            for (entry in arrayOfList[i]) {
+                outputString += "$entry"
+            }
+            outputString += "\n"
+        }
+        return outputString
+    }
 }
---
+++ src/test/kotlin/spbuhomework/hw4/task1/HashTableTest.kt
@@ -0,0 +1,113 @@
+package spbuhomework.hw4.task1
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import java.lang.IllegalArgumentException
+import kotlin.math.pow
+
+internal class HashTableTest {
+    @Test
+    fun put() {
+        /* just checking if it doesn't crash
+        * put will be tested with other functions*/
+        val myHashTable = HashTable<String, String>()
+        for (i in 1..100) {
+            myHashTable.put(i.toString(), i.toString())
+        }
+    }
+
+    @Test
+    fun getSize_nonEmptyHashTable_correctOutput() {
+        val myHashTable = HashTable<String, String>()
+        for (i in 1..100) {
+            myHashTable.put(i.toString(), i.toString())
+        }
+        assertEquals(100, myHashTable.size)
+    }
+
+    @Test
+    fun getSize_emptyHashTable_zeroAsOutput() {
+        val myHashTable = HashTable<String, String>()
+        assertEquals(0, myHashTable.size)
+    }
+
+    @Test
+    fun get_emptyHashTable_exceptionThrown() {
+        val myHashTable = HashTable<String, String>()
+        assertThrows(IndexOutOfBoundsException::class.java) {
+            myHashTable.get("two number 9s, a number 9 large, a number 6 with extra dip...")
+        }
+    }
+
+    @Test
+    fun get_nonEmptyHashTableButItemDoesNotExist_exceptionThrown() {
+        val myHashTable = HashTable<String, String>()
+        for (i in 1..10) {
+            myHashTable.put(i.toString(), i.toString())
+        }
+        myHashTable.put("Kotlin", "is cool")
+        assertThrows(IndexOutOfBoundsException::class.java) {
+            myHashTable.get("Java")
+        }
+    }
+
+    @Test
+    fun get_nonEmptyHashTableAndItemExist_correctOutput() {
+        val myHashTable = HashTable<String, String>()
+        for (i in 1..10) {
+            myHashTable.put(i.toString(), i.toString())
+        }
+        myHashTable.put("Kotlin", "is cool")
+        assertEquals("is cool", myHashTable.get("Kotlin"))
+    }
+
+    @Test
+    fun isContains_itemDoesNotExist_falseAsOutput() {
+        val myHashTable = HashTable<String, String>()
+        for (i in 1..100) {
+            myHashTable.put(i.toString(), (i + 5).toString())
+        }
+        assertFalse(myHashTable.isContains("135"))
+    }
+
+    @Test
+    fun isContains_itemExist_trueAsOutput() {
+        val myHashTable = HashTable<String, String>()
+        for (i in 1..100) {
+            myHashTable.put(i.toString(), (i + 5).toString())
+        }
+        assertTrue(myHashTable.isContains("78"))
+    }
+
+    @Test
+    fun remove_itemExist_itemRemoved() {
+        val myHashTable = HashTable<String, String>()
+        for (i in 1..100) {
+            myHashTable.put(i.toString(), (i + 5).toString())
+        }
+        myHashTable.remove("79")
+        assertFalse(myHashTable.isContains("79"))
+    }
+
+    @Test
+    fun remove_itemDoesNotExist_exceptionThrown() {
+        val myHashTable = HashTable<String, String>()
+        assertThrows(IndexOutOfBoundsException::class.java) {
+            myHashTable.remove("Hello!")
+        }
+    }
+
+    @Test
+    fun setHashFunction_someDataAddedAndHashFunctionChanged_HashTableSuccessfullyRefiled() {
+        val myHashTable = HashTable<String, String>()
+        for (i in 1..100) {
+            myHashTable.put(i.toString(), (i + 5).toString())
+        }
+        myHashTable.hashFunction = ::simpleHashFunction
+        assertEquals("37", myHashTable.get("32"))
+        myHashTable.hashFunction = ::differentHashFunction
+        assertEquals("69", myHashTable.get("64"))
+    }
+
+}
