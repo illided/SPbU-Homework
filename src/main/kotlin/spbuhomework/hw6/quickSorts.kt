1e98e0198b370dec5cd932984e8e3bc4da3699ec
illided <utkin.ilya.nickolaevich@gmail.com>
2020-06-08 12:08:41 +0300

    partition was added in Sort

--- src/main/kotlin/spbuhomework/hw6/quickSorts.kt
+++ src/main/kotlin/spbuhomework/hw6/quickSorts.kt
@@ -2,11 +2,32 @@ package spbuhomework.hw6
 
 import kotlinx.coroutines.experimental.runBlocking
 
-interface Sort {
-    fun <T> sortArray(inputArray: Array<T>): Array<T> where T : Comparable<T>
+abstract class Sort {
+    abstract fun <T> sortArray(inputArray: Array<T>): Array<T> where T : Comparable<T>
+
+    fun <T> partition(inputArray: Array<T>, left: Int, right: Int): Int where T : Comparable<T> {
+        var i = left
+        var j = right
+        val middle = inputArray[(left + right) / 2]
+        while (i <= j) {
+            while (inputArray[i] < middle) {
+                i++
+            }
+            while (inputArray[j] > middle) {
+                j--
+            }
+            if (i >= j) {
+                break
+            }
+            inputArray[i] = inputArray[j].also { inputArray[j] = inputArray[i] }
+            i++
+            j--
+        }
+        return j
+    }
 }
 
-class StandardQuickSort : Sort {
+class StandardQuickSort : Sort() {
     override fun <T> sortArray(inputArray: Array<T>): Array<T> where T : Comparable<T> {
         runStandardQuickSort(inputArray, 0, inputArray.size - 1)
         return inputArray
@@ -14,30 +35,14 @@ class StandardQuickSort : Sort {
 
     private fun <T> runStandardQuickSort(inputArray: Array<T>, left: Int, right: Int) where T : Comparable<T> {
         if (left < right) {
-            var i = left
-            var j = right
-            val middle = inputArray[(left + right) / 2]
-            while (i <= j) {
-                while (inputArray[i] < middle) {
-                    i++
-                }
-                while (inputArray[j] > middle) {
-                    j--
-                }
-                if (i >= j) {
-                    break
-                }
-                inputArray[i] = inputArray[j].also { inputArray[j] = inputArray[i] }
-                i++
-                j--
-            }
-            runStandardQuickSort(inputArray, left, j)
-            runStandardQuickSort(inputArray, j + 1, right)
+            val middleIndex = super.partition(inputArray, left, right)
+            runStandardQuickSort(inputArray, left, middleIndex)
+            runStandardQuickSort(inputArray, middleIndex + 1, right)
         }
     }
 }
 
-class AsyncQuickSort : Sort {
+class AsyncQuickSort : Sort() {
     override fun <T> sortArray(inputArray: Array<T>): Array<T> where T : Comparable<T> {
         runBlocking { runAsyncQuickSort(inputArray, 0, inputArray.size - 1) }
         return inputArray
@@ -45,25 +50,9 @@ class AsyncQuickSort : Sort {
 
     private suspend fun <T> runAsyncQuickSort(inputArray: Array<T>, left: Int, right: Int) where T : Comparable<T> {
         if (left < right) {
-            var i = left
-            var j = right
-            val middle = inputArray[(left + right) / 2]
-            while (i <= j) {
-                while (inputArray[i] < middle) {
-                    i++
-                }
-                while (inputArray[j] > middle) {
-                    j--
-                }
-                if (i >= j) {
-                    break
-                }
-                inputArray[i] = inputArray[j].also { inputArray[j] = inputArray[i] }
-                i++
-                j--
-            }
-            runAsyncQuickSort(inputArray, left, j)
-            runAsyncQuickSort(inputArray, j + 1, right)
+            val middleIndex = super.partition(inputArray, left, right)
+            runAsyncQuickSort(inputArray, left, middleIndex)
+            runAsyncQuickSort(inputArray, middleIndex + 1, right)
         }
     }
 }
