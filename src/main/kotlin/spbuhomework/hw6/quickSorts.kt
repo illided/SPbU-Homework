192e61a299e728331da7e8f5633ee2abbc1f7e54
illided <utkin.ilya.nickolaevich@gmail.com>
2020-06-08 11:33:36 +0300

    sorts added

---
+++ src/main/kotlin/spbuhomework/hw6/quickSorts.kt
@@ -0,0 +1,69 @@
+package spbuhomework.hw6
+
+import kotlinx.coroutines.experimental.runBlocking
+
+abstract class Sort {
+    abstract fun <T> sort(inputArray: Array<T>): Array<T> where T : Comparable<T>
+}
+
+class StandardQuickSort : Sort() {
+    override fun <T> sort(inputArray: Array<T>): Array<T> where T : Comparable<T> {
+        runStandardQuickSort(inputArray, 0, inputArray.size - 1)
+        return inputArray
+    }
+
+    private fun <T> runStandardQuickSort(inputArray: Array<T>, left: Int, right: Int) where T : Comparable<T> {
+        if (left < right) {
+            var i = left
+            var j = right
+            val middle = inputArray[(left + right) / 2]
+            while (i <= j) {
+                while (inputArray[i] < middle) {
+                    i++
+                }
+                while (inputArray[j] > middle) {
+                    j--
+                }
+                if (i >= j) {
+                    break
+                }
+                inputArray[i] = inputArray[j].also { inputArray[j] = inputArray[i] }
+                i++
+                j--
+            }
+            runStandardQuickSort(inputArray, left, j)
+            runStandardQuickSort(inputArray, j + 1, right)
+        }
+    }
+}
+
+class AsyncQuickSort : Sort() {
+    override fun <T> sort(inputArray: Array<T>): Array<T> where T : Comparable<T> {
+        runBlocking { runAsyncQuickSort(inputArray, 0, inputArray.size - 1) }
+        return inputArray
+    }
+
+    private suspend fun <T> runAsyncQuickSort(inputArray: Array<T>, left: Int, right: Int) where T : Comparable<T> {
+        if (left < right) {
+            var i = left
+            var j = right
+            val middle = inputArray[(left + right) / 2]
+            while (i <= j) {
+                while (inputArray[i] < middle) {
+                    i++
+                }
+                while (inputArray[j] > middle) {
+                    j--
+                }
+                if (i >= j) {
+                    break
+                }
+                inputArray[i] = inputArray[j].also { inputArray[j] = inputArray[i] }
+                i++
+                j--
+            }
+            runAsyncQuickSort(inputArray, left, j)
+            runAsyncQuickSort(inputArray, j + 1, right)
+        }
+    }
+}
---
+++ src/test/kotlin/spbuhomework/hw6/StandardQuickSortTest.kt
@@ -0,0 +1,37 @@
+package spbuhomework.hw6
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class StandardQuickSortTest {
+    private val basicQuicksort = StandardQuickSort()
+
+    @Test
+    fun basicQuicksort_smallRandomArray_correctOutput() {
+        assertArrayEquals(arrayOf(1, 2, 3, 4, 5), basicQuicksort.sort(arrayOf(4, 3, 5, 2, 1)))
+    }
+
+    @Test
+    fun basicQuickSort_smallAlreadySortedArray_sameOutput() {
+        val myArray = arrayOf(1, 2, 3, 4, 5)
+        assertArrayEquals(myArray, basicQuicksort.sort(myArray))
+    }
+
+    @Test
+    fun basicQuickSort_biggerRandomArray_correctOutput() {
+        val myArray = arrayOf(8, 7, 12, 16, 1, 11, 13, 5, 10, 4, 3, 2, 14, 6, 9, 17, 15, 20, 19, 18)
+        assertArrayEquals(myArray.sorted().toTypedArray(), basicQuicksort.sort(myArray))
+    }
+
+    @Test
+    fun basicQuickSort_biggerAlreadySortedArray_sameOutput() {
+        val myArray = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+        assertArrayEquals(myArray, basicQuicksort.sort(myArray))
+    }
+
+    @Test
+    fun basicQuickSort_emptyArray_emptyArrayOutput() {
+        assertArrayEquals(arrayOf<Int>(), basicQuicksort.sort(arrayOf<Int>()))
+    }
+}
